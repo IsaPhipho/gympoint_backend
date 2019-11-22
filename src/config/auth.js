9bbd151fe763a1e76d38cbc87bafdeec328734c0
IsaPhipho <isaphiladelpho@gmail.com>
2019-11-22 19:47:21 +0000

    chore: starting Gympoint

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '204895aa15118c0062e60a25c74fbd31',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import StudentController from './app/controllers/StudentController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
+
+export default routes;
